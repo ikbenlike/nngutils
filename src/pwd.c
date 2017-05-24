90b51d36f5009786234f0866cad922c377edf5dc
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-05-24 18:52:58 +0200

    adds error handling to pwd

--- src/pwd.c
+++ src/pwd.c
@@ -1,11 +1,16 @@
 #include <unistd.h>
 #include <limits.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 int main(int argc, char **argv){
     char pwd[PATH_MAX];
 
-    getcwd(pwd, sizeof(pwd));
+    if(!getcwd(pwd, sizeof(pwd))){
+        fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
+        return 1;
+    }
     puts(pwd);
     return 0;
 }
