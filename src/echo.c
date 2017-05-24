c8d17681524cf0b805cee5318f0a029850551483
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-05-24 21:02:28 +0200

    fixes echo printing spaces after what it's supposed to echo, and fixes yes by putting the arguments on separate lines like they're supposed to be

--- src/echo.c
+++ src/echo.c
@@ -7,7 +7,10 @@ int main(int argc, char **argv){
         return 0;
     }
     for(int i = 1; i < argc; i++){
-        printf("%s ", argv[i]);
+        printf("%s", argv[i]);
+        if(i < argc - 1){
+            putchar(' ');
+        }
     }
     puts("");
     return 0;
--- src/yes.c
+++ src/yes.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv){
     if(argc == 1){
         while(1){
-            putchar('y');
+            puts("y");
         }
     }
     else {
@@ -11,6 +11,7 @@ int main(int argc, char **argv){
             for(int i = 1; i < argc; i++){
                 printf("%s ", argv[i]);
             }
+            putchar('\n');
         }
     }
     return 0;
