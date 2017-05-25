f74596a01f0a5f1b25cd385cae6939105e68683f
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-05-25 22:34:11 +0200

    fully implements printenv

--- src/printenv.c
+++ src/printenv.c
@@ -4,8 +4,36 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv, char **environ){
-    for(int i = 0; environ[i] != NULL; i++){
-        puts(environ[i]);
+    char *var;
+    int ret = 0;
+    if(argc == 1){
+        for(int i = 0; environ[i] != NULL; i++){
+            puts(environ[i]);
+        }
     }
-    return 0;
+    else if(!strcmp(argv[1], "-0") || !strcmp(argv[1], "--null")){
+        for(int i = 2; i < argc; i++){
+            var = getenv(argv[i]);
+            if(var == NULL){
+                ret = 1;
+                continue;
+            }
+            else {
+                fprintf(stdout, "%s", var);
+            }
+        }
+    }
+    else {
+        for(int i = 1; i < argc; i++){
+            var = getenv(argv[i]);
+            if(var == NULL){
+                ret = 1;
+                continue;
+            }
+            else {
+                fprintf(stdout, "%s\n", var);
+            }
+        }
+    }
+    return ret;
 }
