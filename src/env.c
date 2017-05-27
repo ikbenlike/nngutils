998b8fb96fd91ff61f59ba2430e8986ed1bae7d8
ikbenlike <thewatcheriswatchinu@gmail.com>
2017-05-27 16:38:43 +0200

    attempts to implement env

---
+++ src/env.c
@@ -0,0 +1,91 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+extern char **environ;
+
+struct env_args {
+    bool ignore_env;
+    bool null;
+    char **unset;
+    int nunset;
+    char *command;
+    char **args;
+    int argc;
+};
+
+struct env_args *parse_arg(struct env_args *args, int argc, char **argv){
+    args->nunset = 0;
+    args->unset = calloc(1, sizeof(char*) * argc);
+    args->args = calloc(1, sizeof(char*) * argc);
+    args->argc = 0;
+    for(int i = 1; i < argc; i++){
+        if(!strcmp(argv[i], "-i") || !strcmp(argv[i], "--ignore-environment") || !strcmp(argv[i], "-")){
+            args->ignore_env = true;
+        }
+        else if(!strcmp(argv[i], "-0") || !strcmp(argv[i], "--null")){
+            args->null = true;
+        }
+        else if(!strcmp(argv[i], "-u")){
+            if(i + 1 >= argc){
+                fprintf(stderr, "%s: missing operand after -u\n", argv[0]);
+                exit(1);
+            }
+            else {
+                args->unset[args->nunset++] = argv[i++];
+            }
+        }
+        else {
+            args->command = argv[i++];
+            for(int a = i; a < argc; a++){
+                args->args[args->argc++] = argv[a];
+            }
+            args->args[args->argc] = NULL;
+            if(args->argc == 0){
+                puts("null");
+                args->args[0] = NULL;
+            }
+            break;
+        }
+    }
+    return args;
+}
+
+int main(int argc, char **argv){
+    if(argc == 1){
+        for(int i = 0; environ[i] != NULL; i++){
+            puts(environ[i]);
+        }
+        return 0;
+    }
+    struct env_args *args = calloc(1, sizeof(struct env_args));
+    args = parse_arg(args, argc, argv);
+    puts(args->command);
+    /*if(args->argc != 0){
+        for(int i = 0; i < args->argc; i++){
+            puts(args->args[i]);
+        }
+    }*/
+    if(args->ignore_env){
+        *environ = NULL;
+    }
+    if(args->nunset > 0){
+        for(int i = 0; i < args->nunset; i++){
+            if(unsetenv(args->unset[i]) == -1){
+                fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
+                return 1;
+            }
+        }
+    }
+    if(args->args[0] == NULL){
+        puts("null");
+    }
+    if(execvp(args->command, args->args) == -1){
+        fprintf(stderr, "%s: %s\n", argv[0], strerror(errno));
+        return 1;
+    }
+    return 0;
+}
