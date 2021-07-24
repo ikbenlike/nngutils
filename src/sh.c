0a8dce941bcacf9ce1928a024394ac5e6be5ada2
ikbenlike <thewatcheriswatchinu@gmail.com>
2021-07-24 04:24:35 +0200

    add alias

--- src/sh.c
+++ src/sh.c
@@ -20,6 +20,12 @@ enum sh_builtins {
 
 extern char **environ;
 
+struct sh_alias {
+    size_t id;
+    char *name;
+    char *command;
+};
+
 struct sh_args {
     bool interactive;
     bool standard_input;
@@ -33,6 +39,101 @@ struct sh_command {
     char **argv;
     int argc;
 };
+
+
+
+
+typedef struct {
+    size_t len;
+    size_t cursor;
+    struct sh_alias *list;
+} hashmap_vector_t;
+
+typedef struct {
+    size_t size;
+    hashmap_vector_t *map;
+} hashmap_t;
+
+size_t hashmap_standalone_hash(char *inp){
+    size_t output = 0;
+    size_t i = 0;
+    size_t l = strlen(inp);
+    while((i + 1) <= l){
+        output = output*17 + inp[i++];
+    }
+    return output;
+}
+
+size_t hashmap_hash(hashmap_t *hm, char *inp){
+    size_t output = 0;
+    size_t i = 0;
+    size_t l = strlen(inp);
+    while((i + 1) <= l){
+        output = output*17 + inp[i++];
+    }
+    return output % hm->size;
+}
+
+hashmap_t *hashmap_init(size_t size){
+    hashmap_t *hm = calloc(1, sizeof(hashmap_t));
+    hm->map = calloc(size, sizeof(hashmap_vector_t));
+    hm->size = size;
+    for(size_t i = 0; i < hm->size; i++){
+        hm->map[i].len = 3;
+        hm->map[i].cursor = 0;
+        hm->map[i].list = calloc(1, sizeof(struct sh_alias));
+    }
+    return hm;
+}
+
+int hashmap_free(hashmap_t *hm){
+    for(int i = 0; i < hm->size; i++){
+        free(hm->map[i].list);
+    }
+    free(hm->map);
+    free(hm);
+    return 0;
+}
+
+int hashmap_set(hashmap_t *hm, char *name, char *command){
+    size_t c = hashmap_standalone_hash(name);
+    size_t n = hashmap_hash(hm, name);
+    hm->map[n].list[hm->map[n].cursor].id = c;
+    hm->map[n].list[hm->map[n].cursor].name = name;
+    hm->map[n].list[hm->map[n].cursor++].command = command;
+    return 0;
+}
+
+ssize_t hashmap_item_exists(hashmap_t *hm, char *inp){
+    size_t c = hashmap_standalone_hash(inp);
+    size_t n = hashmap_hash(hm, inp);
+    hashmap_vector_t a = hm->map[n];
+    for(size_t i = 0; i < a.len; i++){
+        if(a.list[i].id == c){
+            return i;
+        }
+    }
+    return -1;
+}
+
+struct sh_alias *hashmap_get(hashmap_t *hm, char *name){
+    if(hashmap_item_exists(hm, name) != -1){
+        size_t id = hashmap_standalone_hash(name);
+        hashmap_vector_t v = hm->map[hashmap_hash(hm, name)];
+        for(size_t i = 0; i < v.len; i++){
+            if(v.list[i].id == id){
+                return &v.list[i];
+            }
+        }
+    }
+    return NULL;
+}
+
+hashmap_t *alias_hashmap;
+
+
+
+
 
 void sigint_handler(int sig){
     write(STDOUT_FILENO, "\n", 1);
@@ -111,12 +212,70 @@ int sh_is_builtin(char *command){
         return shb_cd;
     }
     else if(!strcmp(command, "alias")){
+        puts("alias");
         return shb_alias;
     }
     else if(!strcmp(command, "unalias")){
         return shb_unalias;
     }
     return -1;
+}
+
+int sh_add_alias(char *arg){
+    size_t n_len = 0;
+    size_t c_len = 0;
+    char *name = NULL;
+    char *command = NULL;
+    
+    char *split = strchr(arg, '=');
+
+    if(split){
+        n_len = split - arg;
+        name = calloc(n_len + 1, sizeof(char));
+        strncpy(name, arg, n_len);
+        name[n_len] = '\0';
+
+        c_len = strlen(arg) - n_len;
+        command = calloc(c_len + 1, sizeof(char));
+        strncpy(command, arg + n_len + 1, c_len);
+    }
+
+
+    printf("alias invoked with '%s' and '%s'\n", name, command);
+
+    char *p_name = NULL;
+
+    if(name[0] == '\'' && name[n_len - 1] == '\''){
+        p_name = calloc(n_len - 1, sizeof(char));
+        strncpy(p_name, name + 1, n_len);
+        p_name[n_len - 1] = '\0';
+    }
+    else if(name[0] == '\'' && name[n_len - 1] != '\''){
+        p_name = calloc(n_len, sizeof(char));
+        strncpy(p_name, name + 1, n_len);
+    }
+    else if(name[0] != '\'' && name[n_len - 1] == '\''){
+        p_name = calloc(n_len, sizeof(char));
+        strncpy(p_name, name, n_len - 1);
+        p_name[n_len - 1] = '\0';
+    }
+
+    char *p_command = NULL;
+
+    if(command[0] == '\'' && name[c_len - 1] == '\''){
+        p_command = calloc(c_len - 1, sizeof(char));
+        strncpy(p_command, command, c_len);
+        p_command[c_len - 1] = '\0';
+    }
+    else if(command[0] == '\'' && name[n_len - 1] != '\''){
+        p_command = calloc(c_len, sizeof(char));
+        strncpy(p_command, name + 1, c_len);
+    }
+
+    printf("aliased '%s' to '%s'\n", p_name, p_command);
+
+    return hashmap_set(alias_hashmap, p_name, p_command);
+
 }
 
 int sh_cd(char *path){
@@ -153,6 +312,15 @@ int run_builtin(enum sh_builtins code, struct sh_command *command){
     else if(code == shb_cd){
         return sh_cd(command->argv[1]);
     }
+    else if(code == shb_alias){
+        if(command->argc == 2){
+            return sh_add_alias(command->argv[1]);
+        }
+        else {
+            puts("else");
+            return 1;
+        }
+    }
     return 0;
 }
 
@@ -188,6 +356,7 @@ int call_command(struct sh_command *command){
 
 int main(int argc, char **argv){
     signal(SIGINT, sigint_handler);
+    alias_hashmap = hashmap_init(100);
     struct sh_command *command = NULL;
     if(argc > 1){
         struct sh_args *args = calloc(1, sizeof(struct sh_args));
